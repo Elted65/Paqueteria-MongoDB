7d02096070531ed91ed715f0496b747847da285f
Hector Isaac Ortiz Rosas <hiortiz@outlook.com>
2024-06-27 21:02:47 -0600

    cambio de referencia

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 const route = Router();
 
-route.get('/layout', (req, res) => res.render('layout'));
+route.get('/', (req, res) => res.render('layout'));
 
 export default route;
